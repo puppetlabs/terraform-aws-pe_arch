fa31c9e152e5bf2e02d9fb603106e01cd7c64426
Dimitri Tischenko <dimitri@puppet.com>
2020-03-18 18:50:43 +0100

    healthcheck should be TCP

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -21,7 +21,7 @@ resource "aws_elb" "pe_compiler_elb" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
-    target              = "HTTP:8140/"
+    target              = "TCP:8140"
     interval            = 30
   }
 
