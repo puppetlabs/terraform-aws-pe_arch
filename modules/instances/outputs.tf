43fee34a4cbaab5e64735e02b8d4f8ded74b224b
Dimitri Tischenko <dimitri@puppet.com>
2020-03-18 18:50:35 +0100

    taking the first master for console output

--- modules/instances/outputs.tf
+++ modules/instances/outputs.tf
@@ -1,7 +1,7 @@
 # Output data used by Bolt to do further work, doing this allows for a clean and
 # abstracted interface between cloud provider implementations
 output "console" {
-  value       = aws_instance.master[*].public_ip
+  value       = aws_instance.master[0].public_ip
   description = "This will by the external IP address assigned to the Puppet Enterprise console"
 }
 output "compilers" {
