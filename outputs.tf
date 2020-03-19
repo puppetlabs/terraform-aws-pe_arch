80fafe99a1f4fa2d745a026dee3a7172fd3bd9f0
Dimitri Tischenko <dimitri@puppet.com>
2020-03-19 17:41:48 +0100

    added descriptions to outputs

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,9 @@
 # abstracted interface between cloud provider implementations
 output "console" {
   value       = module.instances.console
-  description = "This will by the external IP address assigned to the Puppet Enterprise console"
+  description = "This will be the external IP address assigned to the Puppet Enterprise console"
 }
 output "pool" {
   value       = module.loadbalancer.lb_dns_name
-  description = "The GCP internal network FQDN of the Puppet Enterprise compiler pool"
+  description = "The AWS loadbalancer's FQDN of the Puppet Enterprise compiler pool"
 }
