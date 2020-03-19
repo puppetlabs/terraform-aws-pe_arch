d7a099f809e2a6a0f923bcf6db1f0b236472a1a3
Dimitri Tischenko <dimitri@puppet.com>
2020-03-19 17:45:48 +0100

    removed zones var

--- modules/loadbalancer/variables.tf
+++ modules/loadbalancer/variables.tf
@@ -3,6 +3,5 @@ variable ports {}
 variable subnet_ids {}
 variable security_group_ids {}
 variable region {}
-# variable zones      {}      
 variable instances {}
 variable project {}
--- variables.tf
+++ variables.tf
@@ -23,16 +23,19 @@ variable "region" {
   type        = string
   default     = "eu-central-1"
 }
-variable "zones" {
-  description = "AWS availability zones that are within the defined AWS region that you wish to use"
-  type        = list(string)
-  default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
-}
+# Not used in the AWS implementation
+# All available AWS availability zones in the region are used automatically"
+# variable "zones" {
+#   description = "AWS availability zones that are within the defined AWS region that you wish to use. Actually ignored in the current implementation - all available zones in the region are used automatically"
+#   type        = list(string)
+#   default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+# }
 variable "compiler_count" {
   description = "The quantity of compilers that are deployed behind a load balancer and will be spread across defined zones"
   type        = number
   default     = 3
 }
+# Note that you might need to accept the AWS EULA for the AMI product used here
 variable "instance_image" {
   description = "The AMI name pattern to use when deploying new cloud instances"
   type        = string
